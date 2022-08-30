c61ea6ec2a235d37bedae639ddbba2bcda7308a1
Dominik Lewandowski <domin5892@gmail.com>
2022-08-30 15:27:14 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gog-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gog';
+}
